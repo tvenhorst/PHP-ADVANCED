aade094dbc5cdad11e7a1a54092a63e982101995
Thomas Venhorst <thomas.w.venhorst@gmail.com>
2022-02-12 10:10:08 +0100

    done

--- 3-Forms/taak01/mijngegevens.php
+++ 3-Forms/taak01/mijngegevens.php
@@ -1,31 +1,16 @@
 <?php
+echo $_POST['Voornaam'] . "<br>";
 
-if( $_POST["voornaam"] ){
-    echo $_POST['voornaam'];
-    echo "<br>";
-} 
-if( $_POST["achternaam"] ){
-    echo $_POST['achternaam'];
-    echo "<br>";
-}
-if( $_POST["klas"] ){
-    echo $_POST['klas'];
-    echo "<br>";
-}
-if( $_POST["leeftijd"] ){
-    echo $_POST['leeftijd'];
-    echo "<br>";
-}
-if( $_POST["adres"] ){
-    echo $_POST['adres'];
-    echo "<br>";
-}
-if( $_POST["plaatsnaam"] ){
-    echo $_POST['plaatsnaam'];
-    echo "<br>";
-}
-if( $_POST["favorietemuziekband"] ){
-    echo $_POST['favorietemuziekband'];
-    echo "<br>";
-}
+echo $_POST['Achternaam'] . "<br>";
+
+echo $_POST['Klas'] . "<br>";
+
+echo $_POST['Leeftijd'] . "<br>";
+
+echo $_POST['Adres'] . "<br>";
+
+echo $_POST['plaatsnaam,'] . "<br>";
+
+echo $_POST['favorietemuziekband,'] . "<br>";
+
 ?>
