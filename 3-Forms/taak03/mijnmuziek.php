bd9494a8ce39fb903909f7a1339d34ab91f4069e
Thomas Venhorst <thomas.w.venhorst@gmail.com>
2022-02-14 12:57:57 +0100

    done

---
+++ 3-Forms/taak03/mijnmuziek.php
@@ -0,0 +1,23 @@
+<!-- invoer.php?Artiest=kanye+west&Titel=civil+war&Jaar_uitkomst=1865&Muzieksoort=piano -->
+
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title></title>
+ </head>
+
+ <body>
+  <form method="GET" action="invoer.php">
+
+      Artiest <br> <input name="Artiest"> <br><br>
+      Titel <br> <input name="Titel"> <br><br>
+      Jaar van uitkomst <br> <input name="Jaar_uitkomst"> <br><br>
+      Muzieksoort <br> <input name="Muzieksoort"> <br><br>
+      <button>Send</button>
+  </form>
+
+ </body>
+</html>
